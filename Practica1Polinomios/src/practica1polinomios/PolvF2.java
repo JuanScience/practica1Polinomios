b54f8ac709a8ab3c9341d87eee333453eb441941
user <IronDev>
2020-03-06 13:57:41 -0500

    beta

--- Practica1Polinomios/src/practica1polinomios/PolvF2.java
+++ Practica1Polinomios/src/practica1polinomios/PolvF2.java
@@ -27,7 +27,7 @@ public class PolvF2 extends PolinomioVector{
         PolvF2 p = new PolvF2(terms); //vector en forma 2
         //ordenar polinomio
         int a;
-        for(int i = 1; i < vec.length - 2; i = i + 2){
+        for(int i = 1; i < vec.length - 2; i = i + 2){//Ordena el vector
             for(int j = 1; j < vec.length - 2; j = j + 2){
                 int x = vec[i]; int y = vec[i + 2];
                 if(vec[j] < vec[j + 2]){
@@ -131,7 +131,7 @@ public class PolvF2 extends PolinomioVector{
                 j++;
         }
         if(j != 0){
-            PolvF2 R = new PolvF2((int)vec[0] - j);//Crea el objeto polinomio con el nuevo número de términos
+            PolvF2 R = new PolvF2(getTam() - j);//Crea el objeto polinomio con el nuevo número de términos
             int k = 1;
             for (int i = 1; i < vec.length; i = i + 2) {//copia los datos útiles al nuevo vector
                 if(vec[i] != 0){
@@ -140,8 +140,8 @@ public class PolvF2 extends PolinomioVector{
                     k = k + 2;
                 }
             }
-            termino0 = termino0 - j;
-            vec = R.vec;//Asigna el nuevo vector al objeto original
+            this.termino0 = R.getTam();
+            this.vec = R.vec;//Asigna el nuevo vector al objeto original
         }
     }
     
@@ -165,5 +165,100 @@ public class PolvF2 extends PolinomioVector{
         vec = R.vec;
     }
     
+    public void eliminarTerm(int exp){
+        if (exp <= vec[2]){//Si el término a ingresar es menor que el grado del polinomio
+            for(int i = 2; i < vec.length; i = i + 2){
+                if (vec[i] == exp){
+                    vec[i - 1] = 0;
+                    vec[i] = 0;
+                }
+            }
+            this.ajustar();
+        }
+    }
+    
+    //Suma dos polinomios
+    public PolvF2 sumar (PolvF2 B){
+        int k = 1, j = 1, pos = 1;
+        PolvF2 R = new PolvF2(getTam() + B.getTam()); //Crea el polinomio resultante
+        while ((k < vec.length) && (j < B.vec.length)){ //Recorre ambos vectores
+            if(vec[k + 1] > B.vec[j + 1]){//Si el exponente del primer sumando es mayor
+                R.setDato((int)vec[k], pos);//Asigna el coeficiente
+                R.setDato((int)vec[k + 1], pos + 1);//Asigna el exponente
+                k = k + 2;//Incrementa posición del primer sumando
+            }else{
+                if(B.vec[j + 1] > vec[k + 1]){//Si el exponente del segundo sumando es mayor que el primero
+                    R.setDato(B.getDato(j), pos);//Asigna el coeficiente
+                    R.setDato(B.getDato(j + 1), pos + 1);//Asigna el exponente
+                    j = j + 2;//Incrementa posición del segundo sumando
+                }else{//Si ambos exponetes son iguales
+                    R.setDato((int)vec[k] + B.getDato(j), pos);//Asigna el coeficiente
+                    R.setDato(B.getDato(j + 1), pos + 1);//Asigna el exponente
+                    k = k + 2;//Incrementa posición del primer sumando
+                    j = j + 2;//Incrementa posición del segundo sumando
+                }
+            }
+            pos = pos + 2;//Incrementa la posición del vector resultante
+        }
+        while((k < vec.length)){ //Termina de sumar si quedan términos en el primer sumando
+            R.setDato(vec[k], pos); //Coeficiente
+            R.setDato(vec[k + 1], pos + 1);//Exponente
+            k = k + 2;
+            pos = pos + 2;
+        }
+        while((j < B.vec.length)){ //Termina de sumar si quedan términos en el segundo sumando
+            R.setDato(B.vec[j], pos); //Coeficiente
+            R.setDato(B.vec[j + 1], pos + 1);//Exponente
+            j = j + 2;
+            pos = pos + 2;
+        }
+        R.ajustar();
+        return (R);
+    }
+    
+    //multiplica dos polinomios
+    public PolvF2 multiplicar (PolvF2 B){
+        PolvF2 ps = new PolvF2(B.getTam());
+        PolvF2 pr = new PolvF2(B.getTam());
+        int pos;
+        for (int i = 2; i < vec.length; i = i + 2){
+            pos = 2;
+            ps.clean();
+            for(int j = 2; j < B.vec.length; j = j + 2){
+                ps.setDato(vec[i - 1] * B.vec[j - 1], pos - 1);
+                ps.setDato(vec[i] + B.vec[j], pos);
+                pos = pos + 2;
+            }
+            pr = pr.sumar(ps);
+        }
+        pr.ajustar();
+        return (pr);
+    }
+    
+    //divide dos polinomios
+    public PolvF2 dividir (PolvF2 B){
+        PolvF2 cociente = new PolvF2(getDato(2) + B.getDato(2));//Crea el cociente
+        PolvF2 aux = new PolvF2(getDato(2) + B.getDato(2));
+        PolvF2 residuo = new PolvF2(getTam());
+        float dato;
+        int pos = 1;//Controla la posición del cociente
+        System.arraycopy(vec, 1, residuo.vec, 1, vec.length - 1);
+        while (residuo.getDato(2) >= B.getDato(2)) {            
+            dato = residuo.vec[1] / B.vec[1];
+            cociente.setDato(dato, pos);//Coeficiente
+            cociente.setDato(residuo.vec[2] - B.vec[2], pos + 1);//Exponente
+            for(int j = 1; j < B.vec.length; j = j + 2){//Multiplica el término del cociente por el dividendo
+                dato = -cociente.vec[pos] * B.vec[j];//Coeficiente
+                aux.setDato(dato, j);
+                aux.setDato(cociente.vec[pos + 1] + B.vec[j + 1], j + 1);//Exponente
+            }
+            residuo = residuo.sumar(aux);
+            residuo.ajustar();
+            aux.clean();
+            pos = pos + 2;
+        }
+        cociente.ajustar();
+        return(cociente);
+    }
 
 }
